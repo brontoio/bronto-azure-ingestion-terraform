5465e0a6aabd7491a2b5992271166b3fcd173d80
Benoit Gaudin <benoit@logchat.io>
2025-02-02 22:06:06 +0000

    BRONTO-1358 release GH artefacts

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-VERSION="$1"
-
 ROOT_DIR="$(pwd)"
 SRC_DIR_NAME="bronto_forwarder"
 
-BUILD_DIR="${ROOT_DIR}/build/${VERSION}"
-rm -rf "${BUILD_DIR}"
-mkdir -p "${BUILD_DIR}"
-
 cd "${ROOT_DIR}/${SRC_DIR_NAME}" || exit
-zip -r "${BUILD_DIR}/brontoForwarder" *
-cd -
+zip -r brontoForwarder *
+cd - || exit
 
-echo "Created artefact: ${BUILD_DIR}/brontoForwarder.zip"
+echo "Created artefact: brontoForwarder.zip"
